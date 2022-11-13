d308cefac3a296ad3524cf3539502dfadaa0f1df
Kareem Campbell <kareemd.campbell@gmail.com>
2022-11-12 20:31:45 -0500

    baseline design

--- src/components/webdev/Left3.jsx
+++ src/components/webdev/Left3.jsx
@@ -6,7 +6,7 @@ export default function Left(){
     return(
         <section className="fromleft fromleft3">
             <div className='style style3'>
-                <img src={Code} className="electrical electrical3" />
+                <img src={Code} className="script script1" />
                 <img src={Page} className="code code1" />
             </div>
         </section>
